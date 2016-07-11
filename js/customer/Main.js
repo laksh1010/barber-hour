28d6a7517ea7457667971e25e9d91578125e1022
Leonardo Tegon <ltegon93@gmail.com>
2016-07-11 20:33:29 -0300

    Add notifications

--- js/customer/Main.js
+++ js/customer/Main.js
@@ -8,14 +8,51 @@ import {
 } from 'react-native';
 
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import PushNotification from 'react-native-push-notification';
+import { connect } from 'react-redux';
 
 import Toolbar from '../common/Toolbar';
 import IconTabBar from '../common/IconTabBar';
 import BarberList from './BarberList';
 import HaircutHistory from './HaircutHistory';
 import Profile from './Profile';
+import Pubnub from '../Pubnub';
+import { gcmSenderID } from '../env';
 
-export default class Main extends Component {
+class Main extends Component {
+  componentDidMount() {
+    PushNotification.configure({
+      onRegister: this._onRegister.bind(this),
+      onNotification: this._onNotification.bind(this),
+      senderID: gcmSenderID
+    });
+  }
+
+  _onRegister(registration) {
+    var {isAdmin, token} = this.props;
+
+    if (isAdmin) {
+      Pubnub.enablePushNotificationsOnChannel('admin', registration.token);
+    }
+
+    Pubnub.enablePushNotificationsOnChannel(`customer_${token}`, registration.token);
+  }
+
+  _onNotification(notification) {
+    if (notification.data) {
+      this._showNotification(notification);
+    } else {
+      this._openNotification(notification);
+    }
+  }
+
+  _showNotification(notification) {
+    PushNotification.localNotification(notification);
+  }
+
+  _openNotification(notification) {
+  }
+
   render() {
     return(
       <View style={styles.container}>
@@ -37,6 +74,15 @@ export default class Main extends Component {
     );
   }
 }
+
+function select(store) {
+  return {
+    isAdmin: store.user.admin,
+    token: store.user.token
+  };
+}
+
+export default connect(select)(Main);
 
 var styles = StyleSheet.create({
   container: {
